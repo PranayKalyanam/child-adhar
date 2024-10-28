4151d7d2d931929a657a5266ef1967ad3d4c2ab8
PranayKalyanam <kalyanampranay45@gmail.com>
2024-10-28 12:30:27 +0530

    Update SplashScreenActivity.kt

--- app/src/main/java/com/example/myapplication/SplashScreenActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.kt
@@ -14,7 +14,7 @@ class SplashScreenActivity : AppCompatActivity() {
     Handler(Looper.getMainLooper()).postDelayed({
         val  intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
-    }, 50000)
+    }, 5000)
 
     }
 }

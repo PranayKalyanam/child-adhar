c4c326ca210ff9e8f4ff584f77867319a793e1c6
PranayKalyanam <kalyanampranay45@gmail.com>
2024-10-28 22:07:04 +0530

    Added bottom navigation with fragments for each menu item
    Implemented bottom navigation bar and fragments for each bottom nav item

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Initialize the binding object
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Set the content view using the binding's root view
+        setContentView(binding.root)
+
+val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        NavigationUI.setupWithNavController(bottomNav, navController)
     }
 }

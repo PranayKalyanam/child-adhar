0106e53232361376e826377ac08f5812b413d342
PranayKalyanam <kalyanampranay45@gmail.com>
2024-10-29 22:02:31 +0530

    Added memories page with UI elements and layout

---
+++ app/src/main/java/com/example/myapplication/Adapter/MemoriesItemAdapter.java
@@ -0,0 +1,68 @@
+package com.example.myapplication.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.databinding.MemoriesItemCardBinding;
+
+import java.util.List;
+
+public class MemoriesItemAdapter extends RecyclerView.Adapter<MemoriesItemAdapter.MemoriesViewHolder> {
+    private final List<String> memoryTitle;
+    private final List<String> memoryDate;
+    private final List<Integer> memoryImages;
+    Context context;
+    public MemoriesItemAdapter(List<String> memoryTitle, List<String> memoryDate, List<Integer> memoryImages, Context context) {
+        this.memoryTitle = memoryTitle;
+        this.memoryDate = memoryDate;
+        this.memoryImages = memoryImages;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MemoriesItemAdapter.MemoriesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        MemoriesItemCardBinding memoriesItemCardBinding = MemoriesItemCardBinding.inflate(inflater, parent, false);
+        return new MemoriesViewHolder(memoriesItemCardBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MemoriesItemAdapter.MemoriesViewHolder holder, int position) {
+    holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return memoryImages.size();
+    }
+
+    public class MemoriesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final MemoriesItemCardBinding binding;
+        public MemoriesViewHolder(MemoriesItemCardBinding memoriesItemCardBinding) {
+            super(memoriesItemCardBinding.getRoot());
+            this.binding = memoriesItemCardBinding;
+            binding.getRoot().setOnClickListener(this);
+        }
+
+        public void bind(int position) {
+            String title = memoryTitle.get(position);
+            String date = memoryDate.get(position);
+            Integer image = memoryImages.get(position);
+            binding.memoriesTitleTextView.setText(title);
+            binding.memoriesDateTextView.setText(date);
+            binding.memoriesImageImageView.setImageResource(image);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/Fragment/MemoriesFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragment/MemoriesFragment.kt
@@ -5,56 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.Adapter.MemoriesItemAdapter
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentMemoriesBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MemoriesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MemoriesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentMemoriesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_memories, container, false)
+//        return inflater.inflate(R.layout.fragment_memories, container, false)
+        binding = FragmentMemoriesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MemoriesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MemoriesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Create some static data
+        val titles = listOf("Memory 1", "Memory 2", "Memory 3")
+        val dates = listOf("01 Jan 2022", "15 Mar 2022", "20 Sep 2022")
+        val images = listOf(R.drawable.banner, R.drawable.siblings, R.drawable.banner)
+
+        setUpAdapter(titles, dates, images)
+
+
+    }
+
+    private fun setUpAdapter(titles: List<String>, dates: List<String>, images: List<Int>) {
+// Initialize the adapter with static data
+        val adapter = MemoriesItemAdapter(titles, dates, images, requireContext())
+        // Set up RecyclerView
+        binding.memoriesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.memoriesRecyclerView.adapter = adapter
     }
 }
